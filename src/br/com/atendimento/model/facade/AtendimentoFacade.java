4cd51d95e79a7cf0ca1a40b4096575c9eca3ee69
Flaviano Linux Lenovo <flaviano_leite@yahoo.com.br>
2016-02-23 21:52:19 -0300

    pequenas alterações

--- src/br/com/atendimento/model/facade/AtendimentoFacade.java
+++ src/br/com/atendimento/model/facade/AtendimentoFacade.java
@@ -11,7 +11,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 
 import br.com.atendimento.model.domain.Atendimento;
 import br.com.atendimento.model.domain.Contato;
@@ -31,142 +30,99 @@ public class AtendimentoFacade {
 		return service.listarTodos();
 	}
 	
-	/*@GET
-	@Path("/contatos")
-	public List<Object> listar() {		
-		Contato contato = new Contato();
-		Operadora operadora = new Operadora();
-		operadora.setCategoria("Fixo");
-		operadora.setCodigo(25);
-		operadora.setNome("GVT");		
-
-		contato.setOperadora(operadora);
-		contato.setNome("Jo„o");
-		contato.setData(new Date());
-		contato.setTelefone("31 -64598-8978");
-		List<Object> lista = new ArrayList<Object>();
-		lista.add(contato);
-		return lista;
-	}*/
+	private List<Contato> listaContatos = new ArrayList<Contato>();
+	private List<Operadora> listaOperadora = new ArrayList<Operadora>();
 	
-	@GET
-	@Path("/contatos")
-	public List<Contato> listarContatos() {	
+	
+		
+	public AtendimentoFacade() {
 		Operadora op1 = new Operadora();
 		op1.setCategoria("Fixo");
 		op1.setCodigo(25);
 		op1.setNome("GVT");	
+		listaOperadora.add(op1);
 		
 		Operadora op2 = new Operadora();
 		op2.setCategoria("Celular");
 		op2.setCodigo(14);
 		op2.setNome("Oi");
 		op2.setPreco(1.0);
+		listaOperadora.add(op2);
 		
 		Operadora op3 = new Operadora();
 		op3.setCategoria("Celular");
 		op3.setCodigo(15);
 		op3.setNome("Vivo");
 		op3.setPreco(2.0);
+		listaOperadora.add(op3);
 		
 		Operadora op4 = new Operadora();
 		op4.setCategoria("Celular");
 		op4.setCodigo(41);
 		op4.setNome("Tim");
 		op4.setPreco(4.0);
+		listaOperadora.add(op4);
 		
 		Operadora op5 = new Operadora();
 		op5.setCategoria("Celular");
 		op5.setCodigo(21);
 		op5.setNome("Embratel");
 		op5.setPreco(5.0);
+		listaOperadora.add(op5);
 		
-		List<Contato> lista = new ArrayList<Contato>();
+		
 		Contato c1 = new Contato();
 		c1.setOperadora(op1);
 		c1.setNome("Carlos AUGUSTO");
 		c1.setData(new Date());
 		c1.setTelefone("31 -12345-8978");
-		lista.add(c1);
+		listaContatos.add(c1);
 		
 		Contato c2 = new Contato();
 		c2.setOperadora(op2);
-		c2.setNome("Jo„o roberto");
+		c2.setNome("Jo√£o roberto");
 		c2.setData(new Date());
 		c2.setTelefone("31 -98574-8978");
-		lista.add(c2);
+		listaContatos.add(c2);
 		
 		Contato c3 = new Contato();
 		c3.setOperadora(op3);
 		c3.setNome("Maria Jose");
 		c3.setData(new Date());
 		c3.setTelefone("31 -85274-8978");
-		lista.add(c3);
+		listaContatos.add(c3);
 		
 		Contato c4 = new Contato();
 		c4.setOperadora(op4);
 		c4.setNome("Flaviano Da Silva");
 		c4.setData(new Date());
 		c4.setTelefone("31 -96585-8978");
-		lista.add(c4);
+		listaContatos.add(c4);
 		
 		Contato c5 = new Contato();
 		c5.setOperadora(op5);
 		c5.setNome("Roberto DA BaHia");
 		c5.setData(new Date());
 		c5.setTelefone("31 -32145-8978");
-		lista.add(c5);
-		
-		return lista;		
+		listaContatos.add(c5);
+	}
+
+	@GET
+	@Path("/contatos")
+	public List<Contato> listarContatos() {
+		return listaContatos;		
 	}
 	
 	@GET
 	@Path("/operadoras")
 	public List<Operadora> listarOperadoras() {
-		
-		List<Operadora> lista = new ArrayList<Operadora>();
-		Operadora op1 = new Operadora();
-		op1.setCategoria("Fixo");
-		op1.setCodigo(25);
-		op1.setNome("GVT");		
-		op1.setPreco(7.0);
-		lista.add(op1);
-		
-		Operadora op2 = new Operadora();
-		op2.setCategoria("Celular");
-		op2.setCodigo(14);
-		op2.setNome("Oi");
-		op2.setPreco(1.0);
-		lista.add(op2);
-		
-		Operadora op3 = new Operadora();
-		op3.setCategoria("Celular");
-		op3.setCodigo(15);
-		op3.setNome("Vivo");
-		op3.setPreco(2.0);
-		lista.add(op3);
-		
-		Operadora op4 = new Operadora();
-		op4.setCategoria("Celular");
-		op4.setCodigo(41);
-		op4.setNome("Tim");
-		op4.setPreco(4.0);
-		lista.add(op4);
-		
-		Operadora op5 = new Operadora();
-		op5.setCategoria("Celular");
-		op5.setCodigo(21);
-		op5.setNome("Embratel");
-		op5.setPreco(5.0);
-		lista.add(op5);
-		
-		return lista;		
+		return listaOperadora;		
 	}
 	
 	@POST
 	@Path("/contatos")
 	public void salvar(Contato contato) {
-		System.out.println("Salvou contato: "+contato);
+		listaContatos.add(contato);
 	}
 		
 
